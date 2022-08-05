2fbc3cb065ace6aeb81168c4a10c20bca37ba0ca
Alexandra-Makarenko <s850272@gmail.com>
2022-08-05 17:31:36 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+// Change code below this line
 console.log(galleryItems);
+const markup = galleryItems
+    .map((item) => 
+  `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`)
+  .join("");
+// console.log(markup);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend',markup)
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'attr',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    captionsData:'alt'
+});
+
