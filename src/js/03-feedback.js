e626c59294d48861737ed7b9fc0ce5d71158cfac
Alexandra-Makarenko <s850272@gmail.com>
2022-08-08 15:07:18 +0300

    fixed 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,36 @@
 import throttle from "lodash.throttle";
 
-const form = document.querySelector(".feedback-form");
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('form');
 let input = document.querySelector("input");
 let textarea = document.querySelector("textarea");
 
+initForm();
 
-const usersData = {};
-
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const formData = new FormData(form);
+    formData.forEach((value, name) => console.log(value, name));
+     input.value = '';
+    textarea.value = '';
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+});
 
 form.addEventListener('input', throttle(evt => {
-    usersData[evt.target.name] = evt.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(usersData));
-}));
+  let usersData = localStorage.getItem(LOCALSTORAGE_KEY);
+  usersData = usersData ? JSON.parse(usersData) : {};
+  usersData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(usersData));
+},500));
 
 
-const savedData = localStorage.getItem("feedback-form-state");
-const parsedData = JSON.parse(savedData);
-
-const loadUsersForm = () => {
-    let items = {};
-    if (savedData) {
-        try {
-    items = parsedData;
-  } catch (error) {
-    console.log(error);
-    }
-    }
-    return items || {};
+function initForm() {
+  let usersData = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (usersData) {
+    usersData = JSON.parse(usersData);
+    Object.entries(usersData).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
 }
-const readUsersForm = () => {
-    const formUsersData = loadUsersForm();
-    input.value = formUsersData.email || '';
-    textarea.value = formUsersData.message || '';
-}
-readUsersForm();
 
-form.addEventListener('submit', clearForm);
-function clearForm(event) {
-    event.preventDefault();
-    localStorage.removeItem("feedback-form-state");
-    console.log('email:', usersData.email || input.value);
-    console.log('message:', usersData.message||textarea.value);
-    input.value = '';
-    textarea.value ='';
-  
-}
